0d524fe3151a4e5197603c245987396d96005ef5
wagaftk15-debug <wagaftk15@gmail.com>
2026-08-23 20:32:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-```python
+
 import os
 import json
 import time
@@ -670,4 +670,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-```
